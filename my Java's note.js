bfdc020e3bd2c330613dec9640d7a750ea12505e
Pedro Henrique Barros <pedrohenriquebarros92@hotmail.com>
2024-09-30 00:24:12 -0300

    Update my Java's note.js

--- my Java's note.js
+++ my Java's note.js
@@ -1,1 +1,16 @@
+# aula1
+ls - listar arquivos
+
+- git starus
+- git add . ou git add tiposPrimitivos.js
+- git commit -m "comentário"
+- git push origin main **primeiro push
+- git push
+
+
+Em caso de problemas no push 
+- git push -f origin main (o f de "force")
+- var com string (nome), tem que ter ; no 1º nome
+- TODA VEZ QUE FOR COLOCAR STRING NO CONSOLE.LOG, COLOCAR CRASE E PARÊNTESES, NO COMEÇO E NO FINAL
+PARA TRANFORMAR EM ANOTAÇÃO OU FECHAR TRABALHO ANTERIOR PARA NÃO PROCESSADO USE /* E *\
 
